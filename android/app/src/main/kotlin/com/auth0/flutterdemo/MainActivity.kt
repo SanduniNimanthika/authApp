2813f5c5e2cba05049b0d59aecb8a5373d56f84d
SanduniNimanthika <thslsanduni1997@gmail.com>
2021-02-16 08:58:29 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/auth0/flutterdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.auth0.flutterdemo
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
